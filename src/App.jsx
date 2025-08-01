f9d9f15e9aea25390856191a82f8516be7401014
sachingour <gour.sachin.sg@gmail.com>
2025-08-01 20:42:30 +0530

    login api setup and redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,26 @@ import Homepage from "./components/Homepage";
 import Login from "./components/Login";
 import LoginSingupWrapper from "./components/LoginSingupWrapper";
 import Singup from "./components/Singup";
+import { Provider } from "react-redux";
+import store from "./utils/store";
 
 function App() {
   return (
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element={<Body />}>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/login" element={<LoginSingupWrapper />}>
-            <Route index element={<Login />} />
+    <Provider store={store}>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/login" element={<LoginSingupWrapper />}>
+              <Route index element={<Login />} />
+            </Route>
+            <Route path="/singup" element={<LoginSingupWrapper />}>
+              <Route index element={<Singup />} />
+            </Route>
           </Route>
-          <Route path="/singup" element={<LoginSingupWrapper />}>
-            <Route index element={<Singup />} />
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,33 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
+  const [emailId, setEmailId] = useState("mona@gmail.com");
+  const [password, setPassword] = useState("Mona@123");
+  const dispatch = useDispatch();
+
+  const loginFormHandler = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:8080/login",
+        { emailId, password },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data));
+    } catch (error) {
+      console.log("Axios error:", error.response?.status, error.message);
+    }
+  };
+
   return (
-    <div>
+    <div className="xl: w-4/5 my-0 mx-auto">
       <h2 className="card-title text-center w-full block font36 font-semibold uppercase mb-5">
         User Login
       </h2>
       <div className="flex gap-2.5 flex-col">
         <fieldset className="fieldset p-0">
-          <legend className="fieldset-legend font20 mb-1 tracking-wide">Email</legend>
+          <legend className="fieldset-legend font20 mb-1 tracking-wide">
+            Email
+          </legend>
           <input
             type="text"
             className="input py-5 font16"
             placeholder="Enter Email"
-            name="email"
+            name="emailId"
+            value={emailId}
+            onChange={(e) => setEmailId(e.target.value)}
           />
         </fieldset>
         <fieldset className="fieldset p-0">
-          <legend className="fieldset-legend font20 mb-1 tracking-wide">Password</legend>
+          <legend className="fieldset-legend font20 mb-1 tracking-wide">
+            Password
+          </legend>
           <input
             type="text"
             className="input py-5 font16 "
             placeholder="Enter Password"
             name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </fieldset>
       </div>
       <div className="card-actions mt-8 mb-6">
-        <button className="btn tracking-wider font16" >Login</button>
+        <button
+          className="btn tracking-wider font16"
+          onClick={loginFormHandler}
+        >
+          Login
+        </button>
       </div>
 
       <p className="font16 leading-3.5 tracking-wide ">
